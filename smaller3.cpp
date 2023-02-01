5c31c0071c59ab61a3b12782690034faeb19d3b8
Irene06he <irene.hehe34@myhunter.cuny.edu>
2023-02-01 14:56:32 -0600

    Add smaller v3

---
+++ smaller3.cpp
@@ -0,0 +1,26 @@
+//Name : Irene He He
+//GitHub name: Irene06he
+
+#include <iostream>
+
+	int main() {
+	int x,y,z;
+  	std::cout<<"Enter the first number:";
+  	std::cin>>x;
+  	std::cout<<"Enter the second number:";
+  	std::cin>>y;
+  	std::cout<<"Enter the third number:";
+  	std::cin>>z;
+  	if (x<=y && x<=z ){
+    	std::cout<<"The smaller of the three is:"<<x<<"\n\n";
+  	}
+  	else if (y<=x && y<=z){
+    	std::cout<<"The smaller of the three is:"<<y<<"\n\n";
+  	}
+  	else {
+    	std::cout<<"The smaller of the three is:"<<z<<"\n\n";
+ 	 }
+	
+	return 0;
+	}
+	
