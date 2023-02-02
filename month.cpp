1d5ea16fd3d875bb0602ca13a43ead00775a5e8e
Irene06he <irene.hehe34@myhunter.cuny.edu>
2023-02-01 21:29:11 -0600

    I modified month

--- month.cpp
+++ month.cpp
@@ -9,20 +9,23 @@ int main(){
   std::cin>>year;
   std::cout<<"Enter month:";
   std::cin>>month;
-  if (month==1 && month==3 && month==5 && month==7 && month==8 && month==10 && month==12){
-    std::cout<<"31 days";
+  if (month==1 ||month==3 || month ==5 || month== 7 || month==8 || month==10 || month==12){
+    std::cout<<"\n\n"<<"31 days";
   }
-  else if(month==4 && month==6 && month==9 && month==11){
-    std::cout<<"30 days";
+  else if (month ==4 || month==6 ||month==9 ||month==11){
+    std::cout<<"\n\n"<<"30 days";
   }
-  else if(year % 4 == 0 && year % 400 == 0 && month==2){
-    std::cout<<"29 days";
+  else if(year % 4 != 0 && month== 2){
+  std::cout<<"\n\n"<<"28 days";
   }
-  else if(year % 100 == 0 && month==2){
-    std::cout<<"28 days";
+  else if (year % 100 != 0 && month==2){
+  std::cout<<"\n\n"<<"29 days";
   }
-  else if (month==2){
-    std::cout<<"28 days";
+  else if (year % 400 != 0 && month==2){
+  std::cout<<"\n\n"<<"28 days";
+  }
+  else if (month ==2){
+  std::cout<<"\n\n"<<"29 days";
   }
 
   return 0;
