b907031ae2d1401e57064b88e894112f635695e8
Irene06he <irene.hehe34@myhunter.cuny.edu>
2023-02-01 20:57:48 -0600

    add month

---
+++ month.cpp
@@ -0,0 +1,29 @@
+//Name : Irene He He
+//GitHub Name: Irene06he
+
+#include <iostream>
+
+int main(){
+  int year,month;
+  std::cout<<"Enter year:";
+  std::cin>>year;
+  std::cout<<"Enter month:";
+  std::cin>>month;
+  if (month==1 && month==3 && month==5 && month==7 && month==8 && month==10 && month==12){
+    std::cout<<"31 days";
+  }
+  else if(month==4 && month==6 && month==9 && month==11){
+    std::cout<<"30 days";
+  }
+  else if(year % 4 == 0 && year % 400 == 0 && month==2){
+    std::cout<<"29 days";
+  }
+  else if(year % 100 == 0 && month==2){
+    std::cout<<"28 days";
+  }
+  else if (month==2){
+    std::cout<<"28 days";
+  }
+
+  return 0;
+}
