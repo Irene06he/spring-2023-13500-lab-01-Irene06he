4a0cc239c6d140fea784d2c7ef4ff84e429d82b4
Irene06he <irene.hehe34@myhunter.cuny.edu>
2023-02-01 21:12:39 -0600

    I modified leap

--- leap.cpp
+++ leap.cpp
@@ -1,20 +1,23 @@
+//Name: Irene He He
+//GitHub Name: Irene06he
+
 #include<iostream>
 
 int main(){
   int year;
-  std::cout<<"Enter year:"<<"\n\n";
+  std::cout<<"Enter year:";
   std::cin>>year;
   if(year % 4 != 0){
-  std::cout<<"Common year";
+  std::cout<<"\n\n"<<"Common year";
   }
   else if (year % 100 != 0){
-  std::cout<<"Leap year";
+  std::cout<<"\n\n"<<"Leap year";
   }
   else if (year % 400 != 0){
-  std::cout<<"Common year";
+  std::cout<<"\n\n"<<"Common year";
   }
   else{
-  std::cout<<"Leap year";
+  std::cout<<"\n\n"<<"Leap year";
   }
   
 
